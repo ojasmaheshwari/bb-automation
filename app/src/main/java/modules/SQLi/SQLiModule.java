b67fef6dac0d5103fdd31d544e450ebe579d8ce5
ojasmaheshwari <ojasmaheshwari2006@gmail.com>
2025-11-26 10:31:37 +0530

    Add double quote injection as well
    Refactor code

--- app/src/main/java/modules/SQLi/SQLiModule.java
+++ app/src/main/java/modules/SQLi/SQLiModule.java
@@ -26,8 +26,13 @@ class SQLiRequest {
     }
 }
 
+enum QuoteType {
+    singleQuote,
+    doubleQuote
+}
+
 public class SQLiModule implements VulnerabilityModule {
-    private MontoyaApi api;
+    private final MontoyaApi api;
 
     public SQLiModule(final MontoyaApi api) {
         this.api = api;
@@ -39,24 +44,36 @@ public class SQLiModule implements VulnerabilityModule {
     public boolean test(HttpRequestToBeSent request) {
         this.api.logging().logToOutput(Util.formatted("Testing on " + request.url()));
 
+        // Single quote injection
+        boolean singleQuoteInjectionSucceeded = testQuoteInjection(request, QuoteType.singleQuote);
+        if (singleQuoteInjectionSucceeded) return true;
+
+        // Double quote injection
+        boolean doubleQuoteInjectionSucceeded = testQuoteInjection(request, QuoteType.doubleQuote);
+        if (doubleQuoteInjectionSucceeded) return true;
+
+        return false;
+    }
+
+    private boolean testQuoteInjection(final HttpRequestToBeSent request, QuoteType type) {
         // Prepare Single quote injections
-        List<SQLiRequest> singleQuoteInjectionRequests = generateSingleQuoteInjectionRequests(request);
-        if (singleQuoteInjectionRequests.isEmpty()) {
+        List<SQLiRequest> quoteInjectionRequests = generateQuoteInjectionRequests(request, type);
+        if (quoteInjectionRequests.isEmpty()) {
             return false;
         }
 
-        for (var injectionRequest : singleQuoteInjectionRequests) {
+        for (var injectionRequest : quoteInjectionRequests) {
             HttpRequest requestToSend = injectionRequest.request;
 
             HttpRequestResponse sentRequest = api.http().sendRequest(requestToSend);
 
             // Check if succeeded
-            if (checkSingleQuoteInjectionSucceeded(sentRequest)) {
+            if (checkQuoteInjectionSucceeded(sentRequest)) {
                 // Create an issue
                 String detail = String.format("""
-            A single quote was appended to <b>%s</b> parameter of this request and it lead to a internal server error.<br>
+            A %s quote was appended to <b>%s</b> parameter of this request and it lead to a internal server error.<br>
             This might be an indication for a SQL Injection.
-            """, injectionRequest.modifiedParameter);
+            """, (type == QuoteType.singleQuote ? "single quote" : "double quote"), injectionRequest.modifiedParameter);
 
                 AuditIssue issue = AuditIssue.auditIssue(
                         "SQL Injection detected",
@@ -76,14 +93,10 @@ public class SQLiModule implements VulnerabilityModule {
             }
         }
 
-        // Prepare Double quote injections
-
-        // Check if succeeded
-
         return false;
     }
 
-    private List<SQLiRequest> generateSingleQuoteInjectionRequests(final HttpRequestToBeSent request) {
+    private List<SQLiRequest> generateQuoteInjectionRequests(final HttpRequestToBeSent request, QuoteType type) {
         // Inject single quote in query parameters
         this.api.logging().logToOutput(
                 Util.formatted(request.parameters().toString())
@@ -92,7 +105,7 @@ public class SQLiModule implements VulnerabilityModule {
         List<SQLiRequest> newRequests = new ArrayList<>();
 
         for (var parsedHttpParameter : request.parameters()) {
-            String newValue = parsedHttpParameter.value() + "'";
+            String newValue = parsedHttpParameter.value() + (type == QuoteType.singleQuote ? "'" : "\"");
             HttpParameter parameter = HttpParameter.parameter(parsedHttpParameter.name(), newValue, parsedHttpParameter.type());
 
             newRequests.add(
@@ -108,7 +121,7 @@ public class SQLiModule implements VulnerabilityModule {
         return newRequests;
     }
 
-    private boolean checkSingleQuoteInjectionSucceeded(HttpRequestResponse requestResponse) {
+    private boolean checkQuoteInjectionSucceeded(HttpRequestResponse requestResponse) {
         if (requestResponse.response().statusCode() == 500) {
             return true;
         }
